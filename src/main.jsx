cd261bd501911dd22782cd510e29281ca66793b7
bcmclaptop08 <faruk.jsor@gmail.com>
2023-09-20 12:13:47 +0600

    main.jsx file and components folder copyed form anoterh project

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+
+const router = createBrowserRouter([
+
+  {
+    path: '/',
+    element: <Home></Home>
+  },
+  {
+    path: '/about',
+    element: <div>This is about page</div>
+  },
+  {
+    path: '/contact',
+    element: <div>This is a contact page</div>
+  }
+]);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
